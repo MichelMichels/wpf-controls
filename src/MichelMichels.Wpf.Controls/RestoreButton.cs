99966e6bf8904c608571f070b04a8040eb3f453f
Michel Michels <michel@miche.ls>
2023-11-24 22:45:36 +0100

    Add RestoreButton

---
+++ src/MichelMichels.Wpf.Controls/RestoreButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace MichelMichels.Wpf.Controls;
+
+public class RestoreButton : TitlebarButton
+{
+    public RestoreButton()
+    {
+        Click += RestoreButton_Click;
+    }
+
+    static RestoreButton()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(RestoreButton),
+            new FrameworkPropertyMetadata(typeof(RestoreButton)));
+    }
+
+    private void RestoreButton_Click(object sender, RoutedEventArgs e)
+    {
+        Window.GetWindow(this).WindowState = WindowState.Normal;
+    }
+}
